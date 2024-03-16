c07c0812f4ce488e82b5bb4a1d63cedde87835fb
Devil-Rick <saptarshinaruto@gmail.com>
2024-03-17 01:01:07 +0530

    screen updated and functionality added

--- src/Components/dailpad/dailpad.jsx
+++ src/Components/dailpad/dailpad.jsx
@@ -1,22 +1,75 @@
+import { useState } from "react";
 import Display from "../screen/screen"
 import styles from './dailpad.module.css'
 
 const dailkeys = [['C', '+/-', '%', '/'], [1, 2, 3, '+'], [4, 5, 6, '-'], [7, 8, 9, '*'], [0, '.', '=']];
+let output = '';
 
 const Dailpad = () => {
+    const [input, setInput] = useState(0);
+    const [partial, setPartial] = useState();
+    const [res, setRes] = useState(0);
+
+    const calc = (num, operation) => {
+        switch (operation) {
+            case '+':
+                setRes(res + parseInt(num))
+                break;
+            case '-':
+                setRes(res - parseInt(num))
+                break;
+            case '*':
+                setRes(res * parseInt(num))
+                break;
+            case '/':
+                setRes(res / parseInt(num))
+                break;
+            case '%':
+                setRes((res * parseInt(num))/100)
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    const getkey = (key) => {
+        if (!parseInt(key) && key !== 0) {
+            if (key === 'C') {
+                output = '';
+                setPartial(output);
+                setInput(output);
+                setRes(0);
+            } else if (key === '=') {
+                calc(output, key)
+                output = '';
+                setPartial(output);
+                setInput(output);
+            } else {
+                calc(output, key)
+                output = output.concat(key);
+                setPartial(output);
+                output = '';
+                setInput(output);
+            }
+        } else {
+            output = output.concat(key);
+            setInput(output);
+        }
+    }
+
     return (
         <div className={styles.display}>
-            <Display />
+            <Display value={input} para={partial} result={res} />
             <div className={styles.dial}>
                 <div className={styles.numbers}>
                     {dailkeys.map((row) => (
                         <div>
-                            {row.map((key) => {
-                                if (key === 0) {
-                                    return <button className={styles.zero}>{key}</button>
-                                }
-                                return <button>{key}</button>
-                            })}
+                            {row.map((key) => (
+                                <button className={(key === 0) ? styles.zero : null} onClick={() => getkey(key)}>
+                                    {key}
+                                </button>
+                            ))}
                         </div>
                     ))}
                 </div>
@@ -24,4 +77,5 @@ const Dailpad = () => {
         </div>
     )
 }
+
 export default Dailpad
--- src/Components/screen/screen.jsx
+++ src/Components/screen/screen.jsx
@@ -1,10 +1,13 @@
 import styles from './screen.module.css';
 
-const Display = () => {
+const Display = ({value, para, result}) => {
+
     return (
-        <h2 className={styles.display}>
-            0.00
-        </h2>
+        <div className={styles.display}>
+            <div className={styles.comp} ><p>History</p><p>{(para ? para : 0)}</p></div>
+            <div className={styles.comp}><p>Result</p><p>{(result ? result : 0)}</p></div>
+            <div><h2>{(value ? value : 0)}</h2></div>
+        </div>
     );
 }
 
