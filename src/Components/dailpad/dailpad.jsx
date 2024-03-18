27808c68de0d7deb09f4a56f08776768b87cd620
Devil-Rick <saptarshinaruto@gmail.com>
2024-03-19 00:41:43 +0530

    updated dailpad

--- src/Components/dailpad/dailpad.jsx
+++ src/Components/dailpad/dailpad.jsx
@@ -3,57 +3,76 @@ import Display from "../screen/screen"
 import styles from './dailpad.module.css'
 
 const dailkeys = [['C', '+/-', '%', '/'], [1, 2, 3, '+'], [4, 5, 6, '-'], [7, 8, 9, '*'], [0, '.', '=']];
-let output = '';
+
+let output = 0;
+let result = 0;
+let calculate = false;
 
 const Dailpad = () => {
     const [input, setInput] = useState(0);
-    const [partial, setPartial] = useState();
+    const [partial, setPartial] = useState(0);
     const [res, setRes] = useState(0);
 
     const calc = (num, operation) => {
         switch (operation) {
             case '+':
-                setRes(res + parseInt(num))
+                console.log(result, operation, output);
+                result += num;
                 break;
             case '-':
-                setRes(res - parseInt(num))
+                console.log(result, operation, output);
+                result -= num;
                 break;
             case '*':
-                setRes(res * parseInt(num))
+                console.log(result, operation, output);
+                result *= num;
                 break;
             case '/':
-                setRes(res / parseInt(num))
+                console.log(result, operation, output);
+                result /= num;
                 break;
             case '%':
-                setRes((res * parseInt(num))/100)
+                console.log(result, operation, output);
+                result = (result * num) / 100
                 break;
+            case '=':
+                return setRes(result)
             default:
-                break;
+                return 0;
         }
     }
 
-
+//  Major Change in get Key is required
     const getkey = (key) => {
-        if (!parseInt(key) && key !== 0) {
-            if (key === 'C') {
-                output = '';
-                setPartial(output);
-                setInput(output);
-                setRes(0);
-            } else if (key === '=') {
-                calc(output, key)
-                output = '';
-                setPartial(output);
-                setInput(output);
-            } else {
-                calc(output, key)
-                output = output.concat(key);
-                setPartial(output);
-                output = '';
-                setInput(output);
+        if (!parseInt(key) && key !== 0 && key !== '.') {
+            if (typeof output === 'string') {
+                output = parseFloat(output)
             }
+            if (key === 'C') {
+                result = 0
+                calculate = false
+                setPartial(0);
+                setInput(0);
+                setRes(0);
+            } else if (key === '+/-') {
+                console.log('Invert');
+            } else {
+                if (calculate === false) {
+                    calculate = true
+                    console.log('True is set'); 
+                }else{
+                    console.log(calc(output, key));
+                }    
+            }
+            output = 0;
         } else {
-            output = output.concat(key);
+            if (key === '.') {
+                output = output.toString() + '.';
+            } else if (typeof output === 'string') {
+                output = output + key
+            } else {
+                output = output * 10 + key;
+            }
             setInput(output);
         }
     }
